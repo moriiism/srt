45941d03fe61b021aad6841b5a4958358da8d7a6
Mikio Morii <morii.mikio@gmail.com>
2022-07-06 10:05:13 +0000

    mod

---
+++ richlucy_bg2_smth/arg_richlucy_bg2_smth.cc
@@ -0,0 +1,160 @@
+#include "arg_richlucy_bg2_smth.h"
+
+// public
+
+void ArgValRichlucyBg2Smth::Init(int argc, char* argv[])
+{
+    progname_ = "richlucy_bg2_smth";
+
+    option long_options[] = {
+        {"debug",       required_argument, NULL, 'd'},
+        {"help",        required_argument, NULL, 'h'},
+        {"verbose",     required_argument, NULL, 'v'},
+        {0, 0, 0, 0}
+    };
+
+    // long option default
+
+    SetOption(argc, argv, long_options);
+    
+    printf("ArgVal::Init: # of arg = %d\n", argc - optind);
+    int narg = 18;
+    if (argc - optind != narg){
+        printf("# of arguments must be %d.\n", narg);
+        Usage(stdout);
+    }
+    int iarg = optind;
+    datafile_       = argv[iarg]; iarg++;
+    bgfile_         = argv[iarg]; iarg++;
+    skyfile_        = argv[iarg]; iarg++;    
+    resp_file_      = argv[iarg]; iarg++;
+    eff_file_       = argv[iarg]; iarg++;
+    nskyx_          = atoi(argv[iarg]); iarg++;
+    nskyy_          = atoi(argv[iarg]); iarg++;
+    ndetx_          = atoi(argv[iarg]); iarg++;
+    ndety_          = atoi(argv[iarg]); iarg++;
+    outdir_         = argv[iarg]; iarg++;
+    outfile_head_   = argv[iarg]; iarg++;
+    nem_            = atoi(argv[iarg]); iarg++;
+    tol_em_         = atof(argv[iarg]); iarg++;
+    npm_            = atoi(argv[iarg]); iarg++;
+    tol_pm_         = atof(argv[iarg]); iarg++;
+    nnewton_        = atoi(argv[iarg]); iarg++;
+    tol_newton_     = atof(argv[iarg]); iarg++;
+    mu_             = atof(argv[iarg]); iarg++;
+}
+
+void ArgValRichlucyBg2Smth::Print(FILE* fp) const
+{
+    fprintf(fp, "%s: g_flag_debug   : %d\n", __func__, g_flag_debug);
+    fprintf(fp, "%s: g_flag_help    : %d\n", __func__, g_flag_help);
+    fprintf(fp, "%s: g_flag_verbose : %d\n", __func__, g_flag_verbose);
+
+    fprintf(fp, "%s: progname_       : %s\n", __func__, progname_.c_str());
+    fprintf(fp, "%s: datafile_       : %s\n", __func__, datafile_.c_str());
+    fprintf(fp, "%s: bgfile_         : %s\n", __func__, bgfile_.c_str());
+    fprintf(fp, "%s: skyfile_        : %s\n", __func__, skyfile_.c_str());    
+    fprintf(fp, "%s: resp_file_      : %s\n", __func__, resp_file_.c_str());
+    fprintf(fp, "%s: eff_file_       : %s\n", __func__, eff_file_.c_str());
+    fprintf(fp, "%s: nskyx_          : %d\n", __func__, nskyx_);
+    fprintf(fp, "%s: nskyy_          : %d\n", __func__, nskyy_);
+    fprintf(fp, "%s: ndetx_          : %d\n", __func__, ndetx_);
+    fprintf(fp, "%s: ndety_          : %d\n", __func__, ndety_);
+    fprintf(fp, "%s: outdir_         : %s\n", __func__, outdir_.c_str());
+    fprintf(fp, "%s: outfile_head_   : %s\n", __func__, outfile_head_.c_str());
+    fprintf(fp, "%s: nem_            : %d\n", __func__, nem_);
+    fprintf(fp, "%s: tol_em_         : %f\n", __func__, tol_em_);
+    fprintf(fp, "%s: npm_            : %d\n", __func__, npm_);
+    fprintf(fp, "%s: tol_pm_         : %f\n", __func__, tol_pm_);
+    fprintf(fp, "%s: nnewton_        : %d\n", __func__, nnewton_);
+    fprintf(fp, "%s: tol_newton_     : %f\n", __func__, tol_newton_);
+    fprintf(fp, "%s: mu_             : %f\n", __func__, mu_);
+}
+
+// private
+
+void ArgValRichlucyBg2Smth::Null()
+{
+    progname_ = "";
+    datafile_ = "";
+    bgfile_   = "";
+    skyfile_  = "";    
+    resp_file_ = "";
+    eff_file_ = "";
+    nskyx_    = 0;
+    nskyy_    = 0;
+    ndetx_    = 0;
+    ndety_    = 0;
+    outdir_   = "";
+    outfile_head_ = "";
+    nem_        = 0;
+    tol_em_     = 0.0;
+    npm_        = 0;    
+    tol_pm_     = 0.0;
+    nnewton_    = 0;    
+    tol_newton_ = 0.0;
+    mu_         = 0.0;
+}
+
+void ArgValRichlucyBg2Smth::SetOption(int argc, char* argv[], option* long_options)
+{
+    if(0 < g_flag_verbose){
+        MPrintInfo("start...");
+    }
+    // option default
+    g_flag_debug   = 0;
+    g_flag_help    = 0;
+    g_flag_verbose = 0;
+    while (1) {
+        int option_index = 0;
+        int retopt = getopt_long(argc, argv, "dhv",
+                                 long_options, &option_index);
+        if(-1 == retopt)
+            break;
+        switch (retopt) {
+        case 0:
+            // long option
+            break;
+        case 'd':
+            g_flag_debug = atoi(optarg);
+            printf("%s: g_flag_debug = %d\n", __func__, g_flag_debug);
+            break;
+        case 'h':
+            g_flag_help = atoi(optarg);
+            printf("%s: g_flag_help = %d\n", __func__, g_flag_help);
+            if(0 != g_flag_help){
+                Usage(stdout);
+            }
+            break;
+        case 'v':
+            g_flag_verbose = atoi(optarg);
+            printf("%s: g_flag_verbose = %d\n", __func__, g_flag_verbose);
+            break;
+        case '?':
+            printf("%s: retopt (= %c) is invalid flag.\n",
+                   __func__, retopt);
+            Usage(stdout);
+            break;
+        default:
+            printf("%s: error: getopt returned character code 0%o ??\n", __func__, retopt);
+            abort();
+        }
+    }
+    if(0 < g_flag_verbose){
+        MPrintInfo("done.");
+    }
+}
+
+
+void ArgValRichlucyBg2Smth::Usage(FILE* fp) const
+{
+    fprintf(fp,
+            "usage: %s [--help (0)] [--verbose (0)] [--debug (0)] "
+            "datafile  bgfile  skyfile  "
+            "resp_file  eff_file  "
+            "nskyx  nskyy  ndetx  ndety  "
+            "outdir  outfile_head  "
+            "nem  tol_em  npm  tol_pm  nnewton  tol_newton  mu\n",
+            progname_.c_str());
+    abort();
+}
---
+++ richlucy_bg2_smth/arg_richlucy_bg2_smth.h
@@ -0,0 +1,81 @@
+#ifndef MORIIISM_SRT_RICHLUCY_BG2_SMTH_ARG_RICHLUCY_BG2_SMTH_H_
+#define MORIIISM_SRT_RICHLUCY_BG2_SMTH_ARG_RICHLUCY_BG2_SMTH_H_
+
+#include "mi_base.h"
+
+class ArgValRichlucyBg2Smth : public MiArgBase{
+public:
+    ArgValRichlucyBg2Smth() :
+        MiArgBase(),
+        progname_(""),
+        datafile_(""),
+	bgfile_(""),
+        skyfile_(""),        
+        resp_file_(""),
+        eff_file_(""),
+        nskyx_(0),
+        nskyy_(0),
+        ndetx_(0),
+        ndety_(0),
+        outdir_(""),
+        outfile_head_(""),
+        nem_(0),
+        tol_em_(0.0),
+        npm_(0),
+        tol_pm_(0.0),
+        nnewton_(0),
+        tol_newton_(0.0),
+        mu_(0.0) {}
+    ~ArgValRichlucyBg2Smth(){
+        Null();
+    }
+    void Init(int argc, char* argv[]);
+    void Print(FILE* fp) const;
+
+    string GetProgname() const {return progname_;};
+    string GetDatafile() const {return datafile_;};
+    string GetBgfile() const {return bgfile_;};
+    string GetSkyfile() const {return skyfile_;};    
+    string GetRespFile() const {return resp_file_;};
+    string GetEffFile() const {return eff_file_;};
+    int    GetNskyx() const {return nskyx_;};
+    int    GetNskyy() const {return nskyy_;};
+    int    GetNdetx() const {return ndetx_;};
+    int    GetNdety() const {return ndety_;};
+    string GetOutdir() const {return outdir_;};
+    string GetOutfileHead() const {return outfile_head_;};
+    int    GetNem() const {return nem_;};
+    double GetTolEm() const {return tol_em_;};
+    int    GetNpm() const {return npm_;};
+    double GetTolPm() const {return tol_pm_;};
+    int    GetNnewton() const {return nnewton_;};
+    double GetTolNewton() const {return tol_newton_;};
+    double GetMu() const {return mu_;};
+    
+private:
+    string progname_;
+    string datafile_;
+    string bgfile_;
+    string skyfile_;
+    string resp_file_;
+    string eff_file_;
+    int nskyx_;
+    int nskyy_;
+    int ndetx_;
+    int ndety_;
+    string outdir_;
+    string outfile_head_;
+    int    nem_;
+    double tol_em_;
+    int    npm_;
+    double tol_pm_;    
+    int    nnewton_;
+    double tol_newton_;
+    double mu_;
+    
+    void Null();
+    void SetOption(int argc, char* argv[], option* long_options);
+    void Usage(FILE* fp) const;
+};
+
+#endif // MORIIISM_SRT_RICHLUCY_BG2_SMTH_ARG_RICHLUCY_BG2_SMTH_H_
